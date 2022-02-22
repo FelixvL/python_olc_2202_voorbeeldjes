8af6407ab3b99d499f08025608cf1fa66c558960
FelixvL <digitaleservice@gmail.com>
2022-02-22 13:48:02 +0100

    xml zit in dataframe

--- verwerkxml/start.py
+++ verwerkxml/start.py
@@ -1,5 +1,22 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 
-xml_data = open('boeken.xml', 'r').read()  # Read file
+#xml_data = open('boeken.xml', 'r').read()  # Read file
 
+deboekendf = pd.read_xml('boeken.xml')
+print(deboekendf.head(5))
+
+
+
+
+
+
+
+
+#root = ET.XML(xml_data)
+
+#data = []
+#cols = []
+
+#for i, child in enumerate(root):
+#    print(i)
